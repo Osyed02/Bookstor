44330747ccf7c8375bbdf20741a6ca3cfaef133f
Osyed_02 <syed.osyed@siliconplex.com>
2022-06-02 17:34:43 +0500

    Admin panel theme added

--- Database/bookstor.sql
+++ Database/bookstor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 27, 2022 at 08:31 AM
+-- Generation Time: Jun 02, 2022 at 12:29 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.27
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `userroles`
+--
+
+CREATE TABLE `userroles` (
+  `id` int NOT NULL,
+  `role` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userroles`
+--
+
+INSERT INTO `userroles` (`id`, `role`) VALUES
+(1, 'user'),
+(2, 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -34,20 +53,27 @@ CREATE TABLE `users` (
   `userName` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
   `Email` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `roleId` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userId`, `firstName`, `lastName`, `userName`, `Email`, `password`, `created_at`) VALUES
-(1, 'test', 'test', 'test', 'test@test.com', 'test', '2022-05-27 13:12:37'),
-(3, 'Osyed', 'Qureshi1', 'Osyed02', 'beosyed@gmail.com', 'osyed12', '2022-05-27 13:13:50');
+INSERT INTO `users` (`userId`, `firstName`, `lastName`, `userName`, `Email`, `password`, `created_at`, `roleId`) VALUES
+(1, 'test', 'test', 'test', 'test@test.com', 'test', '2022-05-27 13:12:37', 1),
+(3, 'Osyed', 'Qureshi1', 'Osyed02', 'beosyed@gmail.com', 'osyed12', '2022-05-27 13:13:50', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `userroles`
+--
+ALTER TABLE `userroles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -55,17 +81,34 @@ INSERT INTO `users` (`userId`, `firstName`, `lastName`, `userName`, `Email`, `pa
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userId`),
   ADD UNIQUE KEY `userName` (`userName`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `roleId` (`roleId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `userroles`
+--
+ALTER TABLE `userroles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `userId` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `userroles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
